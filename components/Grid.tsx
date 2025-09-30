7a62c955e6b69a92f63889cc221a650e33f5e446
Ayush <ayushvishhhhh@gmail.com>
2025-10-01 00:22:12 +0530

    firstCommit

---
+++ components/Grid.tsx
@@ -0,0 +1,12 @@
+import { section } from 'motion/react-client'
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <section id='about'>
+        
+    </section>
+  )
+}
+
+export default Grid
